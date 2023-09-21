d4484235d9d571f901f06f828e198a36d2fecc8d
Dialla23 <diallomahamoudou39@yahoo.fr>
2023-09-21 10:03:05 -0300

    Display City name and main from API

--- index.js
+++ index.js
@@ -4,21 +4,32 @@ let singleDayDiv = document.getElementById('singleday-data')
 let url = " https://api.openweathermap.org/data/2.5/weather?"
 let API_KEY = "a1eafa60d9f83885f917e01f7708985c"
 
-function init(){
+const init= async()=>{
     let cityName = document.getElementById('city-name').value
-    getSingleDayData(cityName)
-  
+    const singleDayData= await getSingleDayData(cityName)
+    // console.log(data)
+    // let cityNamePara= document.createElement('p')
+    // cityNamePara.innerHTML=data.cityName
+    // singleDayDiv.append(cityNamePara)
 }
 
-function getSingleDayData(city){
+const getSingleDayData= async (city)=>{
     let request_url =`${url}q=${city}&appid=${API_KEY}`
+
 
     fetch(request_url).then(res=>res.json()).then(data=>{
         console.log(data)
         let cityNamePara= document.createElement('p')
         cityNamePara.innerHTML= data.name 
-        singleDayDiv.append(cityNamePara) 
-    })
+        singleDayDiv.append(cityNamePara)   
+
+        let main = document.createElement('p')
+        main.innerHTML= data.main.temp
+        singleDayDiv.append(main)
+        return data 
+        
+    }) 
 }
+
 
 searchButton.addEventListener('click',init)
